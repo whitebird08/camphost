9e20d714182be94b4f967dc4f1171443443466b7
Ryan Bird <ryan01bird@gmail.com>
2015-12-18 23:10:17 -0700

    changed text to campground

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -16,20 +16,20 @@ $(document).ready(function() {
           var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<h1 id="firstHeading" class="firstHeading">Campground</h1>'+
             '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            '<p><b>campground</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
             'sandstone rock formation in the southern part of the '+
             'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
             'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+            '(280&#160;mi) by road. Kata Tjuta and campground are the two major '+
+            'features of the campground - Kata Tjuta National Park. campground is '+
             'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
             'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'rock caves and ancient paintings. campground is listed as a World '+
             'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '<p>Attribution: campground, <a href="https://en.wikipedia.org/w/index.php?title=campground&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=campground</a> '+
             '(last visited June 22, 2009).</p>'+
             '</div>'+
             '</div>';
@@ -43,6 +43,7 @@ $(document).ready(function() {
 
         marker.addListener('click', function() {
           infowindow.open(map, marker);
+
         });
 
         }
